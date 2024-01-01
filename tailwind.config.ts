0896e438edaab2e53299f77bea9cc6ab1e38cd86
Bilal Abdullah Gürşen <bilalgursen777@gmail.com>
2024-01-01 17:29:51 +0300

    Theme added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,9 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
-  plugins: [require('daisyui')]
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light', 'dark']
+  }
 }
 export default config
