fcdd7d23c9d514be52ace250259fae76af1f1971
Bilal Abdullah Gürşen <bilalgursen777@gmail.com>
2024-01-01 21:24:39 +0300

    Enhancement issue #1 : Build games page

--- app/oyunlar/page.tsx
+++ app/oyunlar/page.tsx
@@ -1,7 +1,29 @@
+import data from '@/app/data.json'
 export default function Oyunlar() {
   return (
     <>
-      <h1>Oyunlar sayfasıyım</h1>
+      <div className="flex justify-around py-12">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-5">
+          {data.games.map((game) => (
+            <div
+              key={game.id}
+              className="card w-80 bg-base-100 shadow-xl cursor-pointer hover:bg-primary hover:text-primary-content hover:scale-105 duration-150"
+            >
+              <div className="card-body">
+                <h2 className="card-title">
+                  {game.name}
+                  <div className="badge badge-secondary">{game.location}</div>
+                </h2>
+                <p>{game.short_description}</p>
+                <div className="card-actions justify-end">
+                  <div className="badge badge-outline">{game.type}</div>
+                  <div className="badge badge-outline">PS4</div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
