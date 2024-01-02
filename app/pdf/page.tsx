158fa1a1cf42c991a79e54f638d57b39c52c1fb3
Bilal Abdullah Gürşen <bilalgursen777@gmail.com>
2024-01-02 19:00:45 +0300

    Hata fixlendi

--- app/pdf/page.tsx
+++ app/pdf/page.tsx
@@ -1,8 +1,20 @@
+import Link from 'next/link'
+
 export default function Pdf() {
   return (
     <>
       <main className="px-3 lg-px-16 py-8">
-        <h1>ssss</h1>
+        <object
+          data="/PS4.pdf"
+          type="application/pdf"
+          width="100%"
+          height="600px"
+        >
+          <p>
+            PDF dosyasını görüntüleyemiyorsanız,{' '}
+            <Link href={'/PS4.pdf'}>buradan indirebilirsiniz.</Link>
+          </p>
+        </object>
       </main>
     </>
   )
